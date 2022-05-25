6e4092694610e0cc2c3513f3b0d8436c1a3fd825
RenanLeznew <renanw.science@gmail.com>
2022-05-25 13:00:40 -0300

    .

--- jogo-ipc.c
+++ jogo-ipc.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+void checkGreater(int *x, int *y){
+    while(*x > 6 || *y > 8){
+        printf("Esses valores ultrapassam o tamanho do tabuleiro do jogo, insira outros valores de x, y: ");
+        scanf("%d\n%d", &*x, &*y);
+    }
+    while(*x < 1 || *y < 1){
+       printf("Os valores digitados devem ser maiores que 1, insira outros valores de x, y: ");
+        scanf("%d\n%d", &*x, &*y); 
+    }
+}
+
 int main(){
     int gameGrid[6][8];
     int x, y;
@@ -31,14 +42,14 @@ int main(){
     printf("Digite as coordenadas: \n");
     scanf("%d\n%d", &x, &y);
 
-    if(x < 6 && y < 9){
-        while (x != 5 && y != 0)
+    checkGreater(&x, &y);
+        while (x != 6 || y != 0)
         {
             for (int row = 0; row < x; row++)
             {
-                for (int column = y; column < 8; column++)
+                for (int column = y; column < 9; column++)
                 {
-                    gameGrid[row][column] = 0;
+                    gameGrid[row][column-1] = 0;
                 }
                 
             }
@@ -54,22 +65,16 @@ int main(){
             
             printf("Insira outros valores de x, y: ");
             scanf("%d%d", &x, &y);
-            printf("%d\n%d\n", gameGrid[x-1], gameGrid[y+1]);
-            
-            while(gameGrid[x-1] == 0 && gameGrid[y] == 0){
-                printf("Essa coordenada agora eh zero, digite as coordenadas novas: ");
+            checkGreater(&x, &y);
+            while(gameGrid[x-1][y-1] == 0){
+                while(x < 1 || y < 1){
+                    printf("Os valores digitados devem ser maiores que 1, insira outros valores de x, y: ");
+                    scanf("%d\n%d", &x, &y); 
+                }
+                printf("Essa coordenada eh zero, digite as coordenadas novas: ");
                 scanf("%d%d", &x, &y);
             }
-            
         }
-    }
-    else
-    {
-        while(x >= 6 && y>=9){
-            printf("Esses valores ultrapassam o tamanho do tabuleiro do jogo, insira outros valores de x, y:\n ");
-            scanf("%d\n%d", &x, &y);
-        }        
-    }
 
     //todo: nao deixar digitar a coordenada que ja esta com zero
 
